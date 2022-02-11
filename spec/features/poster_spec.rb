dcf2e4ff025aed9d3388887052159a0e914e53ea
Sarah Alexander <sjalex78@gmail.com>
2022-02-11 16:12:11 +1100

    added when then steps

--- spec/features/poster_spec.rb
+++ spec/features/poster_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'new poster' do
-  it 'allows the creation of a science poster, with a title and author name' do
-    #visit the app
+  it 'allows the user to create science poster' do
+    When "a user visits the app" do
     visit root_path
+    end
 
-    #create poster
+    Then "a user can create a new poster" do
     click_on('Create a poster!')
     form = page.find("form[action='#{posters_path}']")
     form.fill_in('poster_title', with: "test_title")
     form.fill_in('poster_author', with: "test_author")
     form.find("input[type='submit']").click
+    end
 
-    #successfully
+    When "a poster is sucessfully created" do
     expect(
       page.find('p#notice')
       .text
     ).to eq "Poster was successfully created."
+    end
 
-    #view the poster as a pdf
+    Then "a user can view the poster as a pdf" do
     click_on('View PDF')
     # TO_DO NEED TO WORK THIS OUT form = page.find("embed[type='application/pdf']")
+    end
   end
 end
