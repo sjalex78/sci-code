0551185e6b774fbd198c5794977329a76795867e
Sarah Alexander <sjalex78@gmail.com>
2022-02-14 16:16:47 +1100

    Add user required for poster

---
+++ db/migrate/20220214051338_add_user_reference_to_posters.rb
@@ -0,0 +1,5 @@
+class AddUserReferenceToPosters < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :posters, :user, null: false, foreign_key: true, type: :uuid
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_013331) do
+ActiveRecord::Schema.define(version: 2022_02_14_051338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2022_02_14_013331) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "css"
+    t.uuid "user_id", null: false
+    t.index ["user_id"], name: "index_posters_on_user_id"
   end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -46,4 +48,5 @@ ActiveRecord::Schema.define(version: 2022_02_14_013331) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posters", "users"
 end
