ab2fba4e8a49c6ff594560c356ff85df8617653d
Sarah Alexander <sjalex78@gmail.com>
2022-02-23 10:02:16 +1100

    fix for devise and rails 7
    using info from [Devise Auth Setup in Rails 7](https://betterprogramming.pub/devise-auth-setup-in-rails-7-44240aaed4be)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
   devise_for :users
   resources :posters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
