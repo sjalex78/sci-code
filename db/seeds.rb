480918c2462c51a4f0b9089b1c921f67bfdb6439
Sarah Alexander <sjalex78@gmail.com>
2022-03-03 13:50:53 +1100

    Updated seed file for components for poster 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,58 +95,85 @@ find_or_create_component_for_poster(
   },
 )
 
-# component_2_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "abstract_sci@poster.com")
-# Component.create(abstract:{ text: "Your abstract (maximum of 250 words) may be
-# incorporated within your poster or attached to your posted and should include
-# the following information about your research project: What was studied?
-# How was it studied?, What were the results?, An explanation of those results.
-# Your abstract should be clear and concise. Avoid abbreviations and jargon. Do
-# not put your name or school information in the body of the abstract. Do not
-# include the title in the body of the abstract.", reference: "High School
-# Research Symposium abstract, poster, and judging information. High School
-# Research Symposium Abstract, Poster, and Judging Information | Center for
-# Mathematics, Science, and Technology - Illinois State. (2022, January 11).
-# Retrieved March 3, 2022, from
-# https://cemast.illinoisstate.edu/students/high-school/research-symposium/abstract-posterrules/"})
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.introduction.sci@poster.com',
+  data: {
+    type: 'introduction',
+    text: <<~EO_TEXT,
+      Present a general overview of what your poster will include. Provide background information
+      necessary to understand the topic. You may also include the research you used
+      to begin your experiment. Explain why you have selected the topic.
+    EO_TEXT
+    reference: <<~EO_TEXT,
+      High School Research Symposium abstract, poster, and judging
+      information. High School Research Symposium Abstract, Poster, and Judging
+      Information | Center for Mathematics, Science, and Technology - Illinois
+      State. (2022, January 11). Retrieved March 3, 2022, from
+      https://cemast.illinoisstate.edu/students/high-school/research-symposium/abstract-posterrules/
+    EO_TEXT
+  },
+)
 
-# # component_3_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "introduction_sci@poster.com")
-# #Component.create(introduction:{ text: "Introduction Present a general
-# overview of what your poster will include. Provide background information
-# necessary to understand the topic. You may also include the research you used
-# to begin your experiment. Explain why you have selected the topic.",
-# reference: "High School Research Symposium abstract, poster, and judging
-# information. High School Research Symposium Abstract, Poster, and Judging
-# Information | Center for Mathematics, Science, and Technology - Illinois
-# State. (2022, January 11). Retrieved March 3, 2022, from
-# https://cemast.illinoisstate.edu/students/high-school/research-symposium/abstract-posterrules/"})
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.introduction.sci@poster.com',
+  data: {
+    type: 'introduction',
+    text: <<~EO_TEXT,
+      Briefly describe the methods and materials used in your research.
+      How did you collect your data? How often?
+    EO_TEXT
+  },
+)
 
-# # component_4_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "method_sci@poster.com")
-# #Component.create(method: {text: "Briefly describe the methods and materials
-# used in your research. How did you collect your data? How often?", image_1:
-# "", image_2: ""})
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.hypothesis.sci@poster.com',
+  data: {
+    type: 'hypothesis',
+    text: 'IF independent variable THEN dependent variable BECAUSE ....',
+  },
+)
 
-# # component_5_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "hypothesis_sci@poster.com")
-# #Component.create(hypothesis: {text: "IF independent variable THEN dependent
-# variable BECAUSE ...."})
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.discussion.sci@poster.com',
+  data: {
+    type: 'discussion',
+    text: <<~EO_TEXT,
+      Present and describe the data.
+      Highlight the "big picture" of the experiment. Use figures (e.g., maps,
+      diagrams, or photographs) and tables. All figures and tables need to be
+      numbered and have a caption. You need to refer to each figure and table in
+      the text of the poster.
+    EO_TEXT
+  },
+)
 
-# component_6_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "discussion_sci@poster.com")
-# #Component.create(discussion: {text: "Present and describe the data.
-# Highlight the "big picture" of the experiment. Use figures (e.g., maps,
-# diagrams, or photographs) and tables. All figures and tables need to be
-# numbered and have a caption. You need to refer to each figure and table in
-# the text of the poster.", image_1: "", image_2: ""})
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.conclusion.sci@poster.com',
+  data: {
+    type: 'conclusion',
+    text: <<~EO_TEXT,
+      Restate the hypothesis and whether or not it was supported. Summarize the
+      research results and provide an explanation for why these results may have
+      occurred.Do not include any "new" information in this section.
+    EO_TEXT
+  },
+)
 
-# # component_7_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "conclusion_sci@poster.com")
-# #Component.create(conclusion: {text: "Restate the hypothesis and whether or
-# not it was supported. Summarize the research results and provide an
-# explanation for why these results may have occurred.Do not include any "new"
-# information in this section."})
-
-# # component_8_uuid = Digest::UUID.uuid_v5(Digest::UUID::DNS_NAMESPACE, "references_sci@poster.com")
-{
-  type: 'references',
-  text: "Anything included in your poster that is not your own, including text,
-  figures, and/or tables, must be referenced. You may use in-text citations or
-  footnotes throughout your poster.Please use either MLA or APA style for the
-  reference list.",
-}
+find_or_create_component_for_poster(
+  poster: poster1,
+  id_namespace: 'poster1.references.sci@poster.com',
+  data: {
+    type: 'references',
+    text: <<~EO_TEXT,
+      Anything included in your poster that is not your own, including text,
+      figures, and/or tables, must be referenced. You may use in-text citations or
+      footnotes throughout your poster.Please use either MLA or APA style for the
+      reference list.
+    EO_TEXT
+  },
+)
